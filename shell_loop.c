9f119b37ef43cc908ca1fa132c073903d2a9c17b
fadygamal11 <fadygamal007@gmail.com>
2023-11-15 10:46:29 -0800

    should work

--- shell_loop.c
+++ shell_loop.c
@@ -5,8 +5,9 @@
  * @info: the parameter & return info struct
  * @av: the argument vector from main()
  *
- * Return: 0 on success, 1 on error, or error code
+ * Return: 0 on sucess, 1 on error
  */
+
 int hsh(info_t *info, char **av)
 {
 	ssize_t r = 0;
@@ -44,18 +45,21 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin command
+ * find_builtin - find a builtin command
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ *          0 if builtin excuted successfuly,
+ *          1 if builtin found but not successful,
+ *          -2 if builtin signal exit()
+ *
  */
+
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
-	builtin_table builtintbl[] = {
+
+	builtin_table builtintb1[] = {
 		{"exit", _myexit},
 		{"env", _myenv},
 		{"help", _myhelp},
@@ -67,11 +71,11 @@ int find_builtin(info_t *info)
 		{NULL, NULL}
 	};
 
-	for (i = 0; builtintbl[i].type; i++)
-		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+	for (i = 0; builtintb1[i].type; i++)
+		if (_strcmp(info->argv[0], builtintb1[i].type) == 0)
 		{
 			info->line_count++;
-			built_in_ret = builtintbl[i].func(info);
+			built_in_ret = builtintb1[i].func(info);
 			break;
 		}
 	return (built_in_ret);
@@ -83,6 +87,7 @@ int find_builtin(info_t *info)
  *
  * Return: void
  */
+
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
@@ -95,11 +100,10 @@ void find_cmd(info_t *info)
 		info->linecount_flag = 0;
 	}
 	for (i = 0, k = 0; info->arg[i]; i++)
-		if (!is_delim(info->arg[i], " \t\n"))
+		if (!is_delim(info->arg[i], "\t\n"))
 			k++;
 	if (!k)
 		return;
-
 	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
 	if (path)
 	{
@@ -120,11 +124,12 @@ void find_cmd(info_t *info)
 }
 
 /**
- * fork_cmd - forks a an exec thread to run cmd
+ * fork_cmd - forks an exec thread to run cmd
  * @info: the parameter & return info struct
  *
  * Return: void
  */
+
 void fork_cmd(info_t *info)
 {
 	pid_t child_pid;
