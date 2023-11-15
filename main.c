3ca463e816bb127eb4775e502b1b5a053fdc34e7
aboodXsama <samal45682@gmail.com>
2023-11-15 10:13:30 -0800

    gl

--- main.c
+++ main.c
@@ -7,16 +7,16 @@
  *
  * Return: 0 on success, 1 on error
  */
-
 int main(int ac, char **av)
 {
-	info_t info[] = { INFO_INT };
+	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
 		: "=r" (fd)
 		: "r" (fd));
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
